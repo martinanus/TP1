74b216b7db0ec23a9aa6d9abc2301f6429b2ebef
Martin <manus@fi.uba.ar>
2021-06-17 18:54:00 -0300

    archivo-compilacion-condicional-

--- All/src/All.c
+++ All/src/All.c
@@ -1,22 +1,18 @@
 #define PROGRAM 0
 
-	#if PROGRAM == TP1_1
-	//#include "Projects/TP1/Blinky/blinky.c"
-
-	#elif PROGRAM==0
-		#define HOLA 0
-	//#include "Projects/TP1/Blinky/blinky.c"
-	#elif PROGRAM == TP1_1
-	//#include "Projects/TP1/Blinky/blinky.c"
-	#elif PROGRAM == TP1_1
-	//#include "Projects/TP1/Blinky/blinky.c"
-	#endif
+#if PROGRAM == TP1_1
+//#include "Projects/TP1/Blinky/blinky.c"
+#elif PROGRAM==0
+//#include "Projects/TP1/Blinky/blinky.c"
+#elif PROGRAM == TP1_1
+//#include "Projects/TP1/Blinky/blinky.c"
+#elif PROGRAM == TP1_1
+//#include "Projects/TP1/Blinky/blinky.c"
+#endif
 
 
 int main(void){
 
-
-	int k = HOLA;
 
 	return 0;
 }
