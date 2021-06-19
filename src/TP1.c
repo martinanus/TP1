2c0b1f1b287312e3927be6db53be9f53a75fb48a
Martin <manus@fi.uba.ar>
2021-06-19 12:45:01 -0300

    define_on_file

--- src/TP1.c
+++ src/TP1.c
@@ -1,15 +1,7 @@
+#define TP1_2
 
-#define PROGRAM TP1_1
 
-
-#define TP1_1 1
-#define TP1_2 2
-#define TP1_3 3
-#define TP1_4 4
-#define TP1_5 5
-#define TP1_6 6
-
-#if PROGRAM==TP1_1
+#ifdef TP1_1
 
 /*==================[inclusions]=============================================*/
 
@@ -59,13 +51,9 @@ int main(void){
 
 /*==================[end of file]============================================*/
 
+#endif
 
-
-
-
-
-
-#elif PROGRAM==TP1_2
+#ifdef TP1_2
 
 /*==================[inclusions]=============================================*/
 
@@ -126,7 +114,9 @@ int main(void){
 }
 
 /*==================[end of file]============================================*/
-#elif PROGRAM==TP1_3
+#endif
+
+#ifdef TP1_3
 
 /*==================[inclusions]=============================================*/
 
@@ -207,9 +197,259 @@ int main(void)
 }
 
 /*==================[end of file]============================================*/
+#endif
+#ifdef PROGRAM
 
-#elif PROGRAM==TP1_4
-#elif PROGRAM==TP1_5
-#elif PROGRAM==TP1_6
+/*==================[inclusions]=============================================*/
+
+#include "sapi.h"         // <= sAPI header
+
+/*==================[macros and definitions]=================================*/
+#define TICKRATE_MS 100
+#define LED_TOGGLE_MS 100
+
+/*==================[internal data declaration]==============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+
+/* FUNCION que se ejecuta cada vez que ocurre un Tick. */
+void myTickHook( void *ptr )
+{
+   static bool_t ledState = OFF;
+
+   gpioMap_t led = (gpioMap_t)ptr;
+
+   if( ledState ) {
+      ledState = OFF;
+   } else {
+      ledState = ON;
+   }
+   gpioWrite( led, ledState );
+}
+
+
+/* FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE RESET. */
+int main(void)
+{
+   /* ------------- INICIALIZACIONES ------------- */
+
+   /* Inicializar la placa */
+   boardConfig();
+
+   /* Inicializar el conteo de Ticks con resolucion de 50ms (se ejecuta
+      periodicamente una interrupcion cada 50ms que incrementa un contador de
+      Ticks obteniendose una base de tiempos). */
+   tickConfig( TICKRATE_MS );
+
+   /* Se agrega ademas un "tick hook" nombrado myTickHook. El tick hook es
+      simplemente una funcion que se ejecutara peri�odicamente con cada
+      interrupcion de Tick, este nombre se refiere a una funcion "enganchada"
+      a una interrupcion.
+      El segundo parametro es el parametro que recibe la funcion myTickHook
+      al ejecutarse. En este ejemplo se utiliza para pasarle el led a titilar.
+   */
+   tickCallbackSet( myTickHook, (void*)LEDR );
+   delay(LED_TOGGLE_MS);
+
+   /* ------------- REPETIR POR SIEMPRE ------------- */
+   while(1) {
+      tickCallbackSet( myTickHook, (void*)LEDG );
+      delay(LED_TOGGLE_MS);
+      tickCallbackSet( myTickHook, (void*)LEDB );
+      delay(LED_TOGGLE_MS);
+      tickCallbackSet( myTickHook, (void*)LED1 );
+      delay(LED_TOGGLE_MS);
+      tickCallbackSet( myTickHook, (void*)LED2 );
+      delay(LED_TOGGLE_MS);
+      tickCallbackSet( myTickHook, (void*)LED3 );
+      delay(LED_TOGGLE_MS);
+      tickCallbackSet( myTickHook, (void*)LEDR );
+      delay(LED_TOGGLE_MS);
+   }
+
+   /* NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa no es llamado
+      por ningun S.O. */
+   return 0 ;
+}
+
+#endif
+#ifdef TP1_5
+
+/*==================[inclusions]=============================================*/
+
+#include "sapi.h"       // <= sAPI header
+
+/*==================[macros and definitions]=================================*/
+#define TOGGLE_TIME_MS 1000
+
+/*==================[internal data declaration]==============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+
+/* FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE RESET. */
+int main(void){
+
+   /* ------------- INICIALIZACIONES ------------- */
+
+   /* Inicializar la placa */
+   boardConfig();
+
+   /* The DEBUG* functions are sAPI debug print functions.
+   Code that uses the DEBUG* functions will have their I/O routed to
+   the sAPI DEBUG UART. */
+   DEBUG_PRINT_ENABLE;
+   /* UART for debug messages. */
+   debugPrintConfigUart( UART_USB, 115200 );
+   debugPrintString( "UART CONFIGURADA! \n" );
+
+
+   /* ------------- REPETIR POR SIEMPRE ------------- */
+   while(1) {
+
+      /* Prendo el led azul */
+      gpioWrite( LEDB, ON );
+      debugPrintString( "LED Prendido\n" );
+
+      delay(TOGGLE_TIME_MS);
+
+      /* Apago el led azul */
+      gpioWrite( LEDB, OFF );
+      debugPrintString( "LED Apagado\n" );
+
+      delay(TOGGLE_TIME_MS);
+
+   }
+
+   /* NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa no es llamado
+      por ningun S.O. */
+   return 0 ;
+}
+
+/*==================[end of file]============================================*/
+
+
+#endif
+#ifdef PROGRAM
+
+
+/*==================[inclusions]=============================================*/
+
+#include "sapi.h"         // <= sAPI header
+
+/*==================[macros and definitions]=================================*/
+#define TICKRATE_MS 100
+#define LED_TOGGLE_MS 500
+
+/*==================[internal data declaration]==============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+
+/* FUNCION que se ejecuta cada vez que ocurre un Tick. */
+void myTickHook( void *ptr )
+{
+   static bool_t ledState = OFF;
+
+   gpioMap_t led = (gpioMap_t)ptr;
+
+   if( ledState ) {
+      ledState = OFF;
+   } else {
+      ledState = ON;
+   }
+   gpioWrite( led, ledState );
+}
+
+
+void doNothing( void *ptr ){}
+
+/* FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE RESET. */
+int main(void)
+{
+   /* ------------- INICIALIZACIONES ------------- */
+
+   /* Inicializar la placa */
+   boardConfig();
+
+   /* Inicializar el conteo de Ticks con resolucion de 50ms (se ejecuta
+      periodicamente una interrupcion cada 50ms que incrementa un contador de
+      Ticks obteniendose una base de tiempos). */
+   tickConfig( TICKRATE_MS );
+
+   /* Se agrega ademas un "tick hook" nombrado myTickHook. El tick hook es
+      simplemente una funcion que se ejecutara peri�odicamente con cada
+      interrupcion de Tick, este nombre se refiere a una funcion "enganchada"
+      a una interrupcion.
+      El segundo parametro es el parametro que recibe la funcion myTickHook
+      al ejecutarse. En este ejemplo se utiliza para pasarle el led a titilar.
+   */
+
+
+   gpioConfig( GPIO0, GPIO_INPUT );
+   gpioConfig( GPIO1, GPIO_OUTPUT );
+   bool_t valor;
+
+   /* ------------- REPETIR POR SIEMPRE ------------- */
+   while(1) {
+
+
+
+
+      valor = !gpioRead( TEC1 );
+
+      if(valor){
+          tickCallbackSet( myTickHook, (void*)LEDG );
+          delay(LED_TOGGLE_MS);
+          tickCallbackSet( myTickHook, (void*)LEDB );
+          delay(LED_TOGGLE_MS);
+          tickCallbackSet( myTickHook, (void*)LED1 );
+          delay(LED_TOGGLE_MS);
+          tickCallbackSet( myTickHook, (void*)LED2 );
+          delay(LED_TOGGLE_MS);
+          tickCallbackSet( myTickHook, (void*)LED3 );
+          delay(LED_TOGGLE_MS);
+          tickCallbackSet( myTickHook, (void*)LEDR );
+          delay(LED_TOGGLE_MS);
+      }
+      else{
+          tickCallbackSet( doNothing, (void*)LEDG );
+      }
+
+
+
+
+
+
+   }
+
+   /* NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa no es llamado
+      por ningun S.O. */
+   return 0 ;
+}
+
+
 #endif
 
